4c926e40c7972567a23490784dcf058a43c5e948
Jones Yu <yuj6@wit.edu>
2019-10-19 00:49:13 -0400

    first commit

---
+++ src/edu/wit/cs/comp1000/PA6b.java
@@ -0,0 +1,31 @@
+/*********************************************************************
+*	Lab 8 - PA6b
+*	YOUR NAME HERE
+*	
+*   COMP1000-XX  (ENTER YOUR SESSION, EITHER 09/10 or 11/12)
+*	10/XX/2019  (UPDATE THE DATE) 
+*
+**********************************************************************
+*	Problem Description
+*
+*	Write a program that reads two numbers from the user and
+*	prints out the sum of those two numbers. (UPDATE THE DESCRIPTION)
+*
+***********************************************************************
+*	Analysis
+*
+*	Inputs:  Two integers, read from the user.
+*
+*	Outputs: The sum of the inputs.
+*
+**********************************************************************/
+
+package edu.wit.cs.comp1000;
+
+public class PA6b {
+
+	public static void main(String[] args) {
+		// TODO: write your code here
+	}
+
+}
